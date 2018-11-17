106d88e4d2d0bb6302ac488beb1ee5cb1557c07c
Mac <mac@MacBook-Pro-2.fios-router.home>
2018-11-16 21:40:17 -0600

    implemented change player active sign, add total scores and winner display

--- chapter4/app.js
+++ chapter4/app.js
@@ -39,21 +39,44 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
     // next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    document.querySelector('player-0-panel').classList.toggle('active');
-    document.querySelector('player-1-panel').classList.toggle('active');
-
-    document.querySelector('.dice').style.display = 'none';
+    nextPlayer();
   }
 
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function () {
+  // add current score to global score
+  scores[activePlayer] += roundScore;
 
+  // update the UI
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+  // check if player won the game
+  if (scores[activePlayer] >= 100) {
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('player-' + activePlayer + '-panel').classList.remove('active');
+  } else {
+    nextPlayer();
+  }
+
+
+
+})
+
+function nextPlayer() {
+  // next player
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
 
 
 
