343599e594e2cae48dcad2dbce540d09dc1a764b
Anna Triebwasser <atriebw@gmail.com>
2019-01-17 11:37:09 -0600

    added winner class

--- chapter4/js/app.js
+++ chapter4/js/app.js
@@ -12,7 +12,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-score = [0, 0];
+scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
@@ -38,22 +38,43 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
     //next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    document.querySelector('.dice').style.display = 'none';
+    nextPlayer();
   }
 })
 
+document.querySelector('.btn-hold').addEventListener('click', function () {
+  //add current score to global
+  scores[activePlayer] += roundScore;
+
+  //update the UI
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  //check if player won the game
+  if (scores[activePlayer] >= 20) {
+    //display "winner"
+    document.querySelector('#name-' + activePlayer).textContent = 'winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  } else {
+    //next player
+    nextPlayer();
+  }
+});
+
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
 
 
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+};
 
 
 
