1d03fa1f2555b08c36cad484743fa4e6cf300741
Anna Triebwasser <atriebw@gmail.com>
2019-01-18 12:10:11 -0600

    finished challenge 3

--- chapter4/challenges/js/app.js
+++ chapter4/challenges/js/app.js
@@ -25,28 +25,40 @@ var lastDice;
 document.querySelector('.btn-roll').addEventListener('click', function () {
   if (gamePlaying) {
     //1. random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
 
     //2. display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
     //3. update the round score only if # was not 1
-    if (dice === 6 && lastDice === 6) {
-      //player looses score
-      scores[activePlayer] = 0;
-      document.querySelector('#score-' + activePlayer).textContent = '0';
-      nextPlayer();
-    } else if (dice !== 1) {
+    if (dice1 !== 1 && dice2 !== 1) {
       //add score
-      roundScore += dice;
+      roundScore += dice1 + dice2;
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
       //next player
       nextPlayer();
     }
-    lastDice = dice;
+
+
+    // if (dice === 6 && lastDice === 6) {
+    //   //player looses score
+    //   scores[activePlayer] = 0;
+    //   document.querySelector('#score-' + activePlayer).textContent = '0';
+    //   nextPlayer();
+    // } else if (dice !== 1) {
+    //   //add score
+    //   roundScore += dice;
+    //   document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    // } else {
+    //   //next player
+    //   nextPlayer();
+    // }
+    // lastDice = dice;
   }
 });
 
@@ -72,7 +84,8 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     if (scores[activePlayer] >= winningScore) {
       //display "winner"
       document.querySelector('#name-' + activePlayer).textContent = 'winner!';
-      document.querySelector('.dice').style.display = 'none';
+      document.getElementById('dice-1').style.display = 'none';
+      document.getElementById('dice-2').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
       gamePlaying = false;
@@ -94,7 +107,8 @@ function nextPlayer() {
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
 
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 };
 
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -104,7 +118,8 @@ function init() {
   roundScore = 0;
   activePlayer = 0;
   gamePlaying = true;
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
   document.getElementById('current-0').textContent = '0';
