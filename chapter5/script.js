d71799c0d90e01f0236afc3779b74e0f9a27089e
Anna Triebwasser <atriebw@gmail.com>
2019-01-20 16:03:31 -0600

    created generic function and used call back functions with it

--- chapter5/script.js
+++ chapter5/script.js
@@ -53,3 +53,73 @@ var jane = Object.create(personProto, {
   }
 });
 */
+
+// Primitive vs objects
+
+// primitive
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+// objects
+var obj1 = {
+  name: 'John',
+  age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// functions
+var age = 27;
+var obj = {
+  name: 'John',
+  city: 'Lisbon'
+};
+
+function change(a, b) {
+  a = 40;
+  b.city = 'San Francisco';
+}
+change(age, obj);
+console.log(age);
+console.log(obj.city);
+console.log(obj.name);
+
+//Passing functions as arguments
+var years = [1990, 1988, 1998, 2000, 2005];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2018 - el;
+}
+
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - (0.067 * el));
+  } else {
+    return -1;
+  }
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
