95d8e71d217659d42bb7d2542b3c6d3c19c2b92a
Anna Triebwasser <atriebw@gmail.com>
2019-01-26 20:49:58 -0600

    intro to closures

--- chapter5/script2.js
+++ chapter5/script2.js
@@ -20,3 +20,19 @@ game();
   var score = Math.random() * 10;
   console.log(score >= 5 - goodLuck);
 })(5);
+
+// lecture CLOSURES
+
+function retirement(retirementAge) {
+  var a = " year left until retirement."
+  return function (birthYear) {
+    var age = 2019 - birthYear;
+    console.log((retirementAge - age) + a);
+  };
+}
+var retirementNormal = retirement(67);
+var retirementEarly = retirement(65);
+
+retirementEarly(1974);
+retirement(67)(1974);
+retirementNormal(1969);
